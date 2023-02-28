0d5449cacc8027d2ec5f2480c8798ca50dbdfa35
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-02-28 12:17:48 +0100

    added mockdata for countys and users mockdata file

---
+++ src/database/mockData/countys.js
@@ -0,0 +1,14 @@
+exports.countys = [
+    {
+        county_id: 1,
+        name: "Stockholm",
+    },
+    {
+        county_id: 2,
+        name: "Vasternorrland",
+    },
+    {
+        county_id: 1,
+        name: "Abisko",
+    },
+]
