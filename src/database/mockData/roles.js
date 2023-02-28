241c86a940c1444a5d90a6c630f782af1ac4179f
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-02-28 13:06:44 +0100

    added mockdata for roles

---
+++ src/database/mockData/roles.js
@@ -0,0 +1,14 @@
+exports.roles = [
+    {
+        role_id: 1,
+        role: 'ADMIN',
+    },
+    {
+        role_id: 2,
+        role: 'COUNTY',
+    },
+    {
+        role_id: 3,
+        role: 'USER',
+    },
+]
