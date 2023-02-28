a5afa03c6a61afe22d73ebfff9935ddcd936f400
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-02-28 12:11:19 +0100

    added mockdata for users

---
+++ src/database/mockData/users.js
@@ -0,0 +1,42 @@
+exports.users = [
+    {
+        user_id: 2,
+        name: "Bob",
+        description: "Big fan of nature",
+        email: "bobby123@mail.com",
+        password: "secret",
+        fk_role_id: 1,
+    },
+    {
+        user_id: 2,
+        name: "Frans",
+        description: "Forest lover",
+        email: "forest_frans@mail.com",
+        password: "secret",
+        fk_role_id: 3,
+    },
+    {
+        user_id: 3,
+        name: "Karen",
+        description: "Mother nature",
+        email: "karen@mail.com",
+        password: "secret",
+        fk_role_id: 3,
+    },
+    {
+        user_id: 1,
+        name: "Anna",
+        description: "Love camping all year around",
+        email: "anna_maja@mail.com",
+        password: "secret",
+        fk_role_id: 1,
+    },
+    {
+        user_id: 1,
+        name: "Boss",
+        description: "Big Boss",
+        email: "boss@mail.com",
+        password: "secret",
+        fk_role_id: 1,
+    }
+]
