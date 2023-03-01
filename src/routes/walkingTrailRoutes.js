98cd5cb0d1f0e7585177b0f78b8a647ebe22736c
jessica tanmateos <jessica_tanmateps@hotmail.com>
2023-03-01 16:04:56 +0100

    change of name in route

--- src/routes/walkingTrailRoutes.js
+++ src/routes/walkingTrailRoutes.js
@@ -1,15 +1,20 @@
-const express = require('express')
-const router = express.Router()
-const { isAuthenticated } = require('../middleware/authenticationMiddleware')
-const { getAllWalkingtrails, getWalkingtrailById, createNewWalkingtrail, deleteWalkingtrailById } = require('../controllers/walkingtrailController')
-const { createNewReview } = require('../controllers/reviewController')
+const express = require("express");
+const router = express.Router();
+const { isAuthenticated } = require("../middleware/authenticationMiddleware");
+const {
+  getAllWalkingtrails,
+  getWalkingtrailById,
+  createNewWalkingtrail,
+  deleteWalkingtrailById,
+} = require("../controllers/walkingtrailController");
+const { createNewReview } = require("../controllers/reviewControllers");
 
-router.get('/', isAuthenticated, getAllWalkingtrails)
-router.get('/:walkingtrailId', isAuthenticated, getWalkingtrailById)
+router.get("/", isAuthenticated, getAllWalkingtrails);
+router.get("/:walkingtrailId", isAuthenticated, getWalkingtrailById);
 
-router.post('/', isAuthenticated, createNewWalkingtrail)
-router.post('/:walkingtrailId/review', isAuthenticated, createNewReview)
+router.post("/", isAuthenticated, createNewWalkingtrail);
+router.post("/:walkingtrailId/review", isAuthenticated, createNewReview);
 
-router.delete('/:walkingtrailId', isAuthenticated, deleteWalkingtrailById)
+router.delete("/:walkingtrailId", isAuthenticated, deleteWalkingtrailById);
 
-module.exports = router
+module.exports = router;
