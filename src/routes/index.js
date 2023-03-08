68e253f631b5d5c24e6de5abf59763d2ff90bea9
Mikael Rönnberg <mikael.ronnberg@medieinstitutet.se>
2023-03-08 15:12:26 +0100

    updated countycontrollers

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const authRoutes = require("./authRoutes");
 const userRoutes = require("./userRoutes");
 const reviewRoutes = require("./reviewRoutes");
 const walkingTrailRoutes = require("./walkingTrailRoutes");
+const countyRoutes = require("./countyRoutes");
 
 router.use("/auth", authRoutes);
 router.use("/user", userRoutes);
 router.use("/review", reviewRoutes);
 router.use("/walkingtrail", walkingTrailRoutes);
+router.use("/county", countyRoutes);
 
 module.exports = router;
