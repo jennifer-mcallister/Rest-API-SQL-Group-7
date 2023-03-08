40631528fb4b4c4557142434dd50a1c82927a5ac
jennifer-mcallister <jennifer_mca@hotmail.com>
2023-03-08 19:59:23 +0100

    cleaned up code. Added controller and route

--- src/routes/reviewRoutes.js
+++ src/routes/reviewRoutes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { isAuthenticated } = require("../middleware/authenticationMiddleware");
 const {
   getAllReviews,
+  getReviewByWalkingtrail,
   getReviewById,
   createNewReview,
   updateReviewById,
@@ -12,6 +13,7 @@ const {
 //REVIEW
 
 router.get("/", isAuthenticated, getAllReviews);
+router.get("/walkingtrail/:walkingtrailName", isAuthenticated, getReviewByWalkingtrail);
 router.get("/:reviewId", isAuthenticated, getReviewById);
 router.post("/", isAuthenticated, createNewReview);
 router.put("/:reviewId", isAuthenticated, updateReviewById);
