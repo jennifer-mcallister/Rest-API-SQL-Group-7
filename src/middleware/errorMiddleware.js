8cf282ae295398d8d9680d40215c24a0ea0811e5
Mikael Rönnberg <mikael.ronnberg@medieinstitutet.se>
2023-03-09 21:21:17 +0100

    more fixes

--- src/middleware/errorMiddleware.js
+++ src/middleware/errorMiddleware.js
@@ -9,29 +9,12 @@ exports.errorMiddleware = (error, req, res, next) => {
 
   if (process.env.NODE_ENV === "development") {
     console.error(error);
-
     customError.message = error.message || "No error message...";
     customError.error = error;
   }
 
   if (error instanceof ValidationError)
     customError.validatonErrors = error.validationErrors;
-
-  /*
-	if (error.name === 'ValidationError') {
-		customError.validatonErrors = Object.values(error.errors).map((item) => item.message)
-		customError.statusCode = 400
-	}
-	// Reformats Mongoose error when a duplicate value is entered for a...
-	// ...field that has the "unique: true" validation
-	// prettier-ignore
-	if (error.code && error.code === 11000) {
-    customError.message = `Duplicate value entered for ${Object.keys(
-      error.keyValue
-    )} field, please choose another value`
-    customError.statusCode = 400
-  }
-	*/ //---------------FRÅGA PETTER OM PRETTIER!!---------------------
 
   if (error.name === "CastError") {
     customError.message = `No item found with id : ${error.value}`;
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -7,9 +7,7 @@ const {
 } = require("../middleware/validation/validationSchemas");
 const { validate } = require("../middleware/validation/validationMiddleware");
 
-//POST /api/v1/auth/register
 router.post("/register", validate(registerSchema), register);
-//POST /api/v1/auth/login
 router.post("/login", validate(loginSchema), login);
 
 module.exports = router;
